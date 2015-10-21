da9a2fb92a3fad661a232dd1fbe048c32a5fc2bb
Carl Philip <carlphilip93@hotmail.com>
2015-10-21 12:17:54 +0200

    Update 4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,7 +5,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,12 +18,26 @@ public class Controller implements Initializable {
     @FXML public TextArea itemDescription;
     @FXML public TextArea bidHistory;
     @FXML public TextField highestBid;
+    @FXML public ImageView itemImage;
+
 
     private Firebase myFirebase = new Firebase("https://biddme.firebaseio.com/");
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Initialize data...");
+        itemDescription.setText("Seller: " + "GetSellerCalle \n" + "Description: \n" + "GetDescription \n" + "Start: " + "GetstartKR \n" + "Item: " + " GetItem");
+        bidHistory.setText("GetBid\nArray/list");
+        highestBid.setText("GetBidDesc");
+        File file = new File("src/img.jpg");
+        Image newImage = new Image(file.toURI().toString());
+        itemImage.setImage(newImage);
         countdown.setText("Time function goes here");
+
     }
+
+
+
+
+
 }
