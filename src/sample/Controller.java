65fd03c1747c1fc6efc65062ed0ffa3ecc36fef9
Carl Philip <carlphilip93@hotmail.com>
2015-10-21 14:57:50 +0200

    Update 4.1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,9 @@
 package sample;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
@@ -10,7 +13,7 @@ import javafx.scene.image.ImageView;
 
 import java.io.File;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class Controller implements Initializable {
 
@@ -22,17 +25,39 @@ public class Controller implements Initializable {
 
 
     private Firebase myFirebase = new Firebase("https://biddme.firebaseio.com/");
+    private List<HashMap<String,Object>> fireBaseItems = new ArrayList<HashMap<String, Object>>();
+    private Map<String, Object> item = new HashMap<String, Object>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Initialize data...");
-        itemDescription.setText("Seller: " + "GetSellerCalle \n" + "Description: \n" + "GetDescription \n" + "Start: " + "GetstartKR \n" + "Item: " + " GetItem");
+        //itemDescription.setText("Seller: " + "GetSellerCalle \n" + "Description: \n" + "GetDescription \n" + "Start: " + "GetstartKR \n" + "Item: " + " GetItem");
         bidHistory.setText("GetBid\nArray/list");
         highestBid.setText("GetBidDesc");
         File file = new File("src/img.jpg");
         Image newImage = new Image(file.toURI().toString());
         itemImage.setImage(newImage);
         countdown.setText("Time function goes here");
+
+        myFirebase.child("Items");
+
+        myFirebase.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapchot : dataSnapshot.getChildren()){
+                    item = postSnapchot.getValue(HashMap.class);
+                    fireBaseItems.add((HashMap<String,Object>) item);
+
+                    System.out.println(dataSnapshot.getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
 
     }
 
